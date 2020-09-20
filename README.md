a3256179177e15b91903c24f69d120e4f2e090ea
李鸿磊 <hanley.lei@gmail.com>
2020-09-20 13:32:00 +0800

    [update]添加删除尾随空格功能

--- README.md
+++ README.md
@@ -2,18 +2,24 @@
 
 ## Feature
 
-将全角符号转换为半角符号
+将全角符号转换为半角符号, 并在中英文之间自动加入空格 (pangu 实现)
 
 | From | To  |
 | ---- | --- |
-| ，   | ,   |
-| 。   | .   |
-| ：   | :   |
-| ？   | ?   |
-| ；   | ;   |
-| 、   | ,   |
-| （   | (   |
-| ）   | )   |
+| ,   | ,   |
+| .   | .   |
+| :   | :   |
+| ?   | ?   |
+| ;   | ;   |
+| ,   | ,   |
+| (   | (   |
+| )   | )   |
+
+## Usage
+
+`⌘ ⇧ P` / `F1` 调出命令面板, 键入 `Half Width Forms Convert`, 选择使用即可.
+
+![himg](https://a.hanleylee.com/HKMS/2020-09-20-050838.png?x-oss-process=style/WaMa)
 
 ## Known Issues
 
@@ -21,9 +27,15 @@
 
 ## Release Notes
 
+- 2020-09-20: 项目创建
+
+## 致谢
+
+- [如何实现一个给中英文间加空格的 VS Code 扩展](http://baurine.github.io/2017/12/27/implement_vscode_extension.html))
+
 ## 开源许可
 
-本仓库的所有代码基于 [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0) 进行分发与使用. 协议全文见 [LICENSE](https://github.com/HanleyLee/HLDeviceKit/blob/master/LICENSE) 文件.
+本仓库的所有代码基于 [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0) 进行分发与使用. 协议全文见 [LICENSE](https://github.com/HanleyLee/Halfwidth-Forms-Converter/blob/master/LICENSE) 文件.
 
 Copyright 2019-2020 HanleyLee
 
--- extension.js
+++ extension.js
@@ -1,10 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode')
 const pangu = require('pangu')
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -12,13 +7,8 @@ const pangu = require('pangu')
 function activate(context) {
     // 插件激活时执行
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "halfwidth-forms-converter" is now active!')
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('convert_to_half.convert', function () {
         const editor = vscode.window.activeTextEditor
         if (!editor) {
@@ -57,22 +47,22 @@ function activate(context) {
                         builder.replace(textLine.range, '')
                     } else {
                         let panguText = pangu.spacing(oriTrimText)
-                        panguText = panguText.replace('，', ',')
-                        panguText = panguText.replace('。', '.')
-                        panguText = panguText.replace('：', ':')
-                        panguText = panguText.replace('？', '?')
-                        panguText = panguText.replace('；', ';')
-                        panguText = panguText.replace('、', ',')
-                        panguText = panguText.replace('（', '(')
-                        panguText = panguText.replace('）', ')')
+                        panguText = panguText.replace('，', ', ')
+                        panguText = panguText.replace('。', '. ')
+                        panguText = panguText.replace('：', ': ')
+                        panguText = panguText.replace('？', '? ')
+                        panguText = panguText.replace('；', '; ')
+                        panguText = panguText.replace('、', ', ')
+                        panguText = panguText.replace('（', ' (')
+                        panguText = panguText.replace('）', ') ')
 
                         builder.replace(textLine.range, panguText)
                     }
                 }
             })
         }
+        vscode.commands.executeCommand('editor.action.trimTrailingWhitespace')
     })
-    // vscode.commands.executeCommand('editor.action.trimTrailingWhitespace')
     context.subscriptions.push(disposable)
 }
 exports.activate = activate
